ed7050b6bbcd6dd0b30037a8f330298cbb2fa0a3
Ilan Pe'er <ilan.peer@benextit.com>
2018-12-22 14:39:46 +0200

    added mapview basic implementation

--- src/map/MapView.js
+++ src/map/MapView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import StepService from '../services/StepService';
-import { Map, TileLayer } from 'react-leaflet';
 import { Sidebar, Tab } from 'react-leaflet-sidebarv2';
 import Editor from '../side/editor/Editor';
 import NavStep from './steps/navStep/NavStep';
-import { MapContext } from './map-context';
+
+import { Map, TileLayer, FeatureGroup } from 'react-leaflet';
+import L from 'leaflet';
+import { EditControl } from 'react-leaflet-draw';
 
 import _ from 'lodash';
 
@@ -12,7 +14,18 @@ import './MapView.css';
 
 const stepService = new StepService();
 const COOREDINATES_DEPTH = 7;
-const center = [32.374, 35.116]
+const center = [32.374, 35.116];
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-shadow.png',
+});
+
+//
+
+let polyline;
 
 class MapView extends Component {
 
@@ -53,13 +66,88 @@ class MapView extends Component {
         }
     }
 
+    /* Draw functions */
+    _onEdited = (e) => {
+
+        let numEdited = 0;
+        e.layers.eachLayer((layer) => {
+            numEdited += 1;
+        });
+        console.log(`_onEdited: edited ${numEdited} layers`, e);
+
+        this._onChange();
+    }
+
+    _onCreated = (e) => {
+        let type = e.layerType;
+        let layer = e.layer;
+        if (type === 'marker') {
+            // Do marker specific actions
+            console.log("_onCreated: marker created", e);
+        }
+        else {
+            console.log("_onCreated: something else created:", type, e);
+        }
+        // Do whatever else you need to. (save to db; etc)
+
+        this._onChange();
+    }
+
+    _onDeleted = (e) => {
+
+        let numDeleted = 0;
+        e.layers.eachLayer((layer) => {
+            numDeleted += 1;
+        });
+        console.log(`onDeleted: removed ${numDeleted} layers`, e);
+
+        this._onChange();
+    }
+
+    _onMounted = (drawControl) => {
+        console.log('_onMounted', drawControl);
+    }
+
+    _onEditStart = (e) => {
+        console.log('_onEditStart', e);
+    }
+
+    _onEditStop = (e) => {
+        console.log('_onEditStop', e);
+    }
+
+    _onDeleteStart = (e) => {
+        console.log('_onDeleteStart', e);
+    }
+
+    _onDeleteStop = (e) => {
+        console.log('_onDeleteStop', e);
+    }
+
     render() {
         return (<section className="MapViewContainer">
             <Map id="map" key="mymap"
                 ref={this.setLeafletMapRef}
                 center={center} zoom={10}
-                onClick={this.onMapClick.bind(this)}
+                //onClick={this.onMapClick.bind(this)}
                 onMouseMove={this.onDrawingMove.bind(this)}>
+                
+                <FeatureGroup ref={(reactFGref) => { this._onFeatureGroupReady(reactFGref); }}>
+                    <EditControl
+                        position='topright'
+                        onEdited={this._onEdited}
+                        onCreated={this._onCreated}
+                        onDeleted={this._onDeleted}
+                        onMounted={this._onMounted}
+                        onEditStart={this._onEditStart}
+                        onEditStop={this._onEditStop}
+                        onDeleteStart={this._onDeleteStart}
+                        onDeleteStop={this._onDeleteStop}
+                        draw={{
+                            rectangle: false
+                        }}
+                    />
+                </FeatureGroup>
                 
                 <Sidebar id="sidebar"
                     collapsed={this.state.collapsed} 
@@ -84,6 +172,33 @@ class MapView extends Component {
                 {this.getNavSteps()}
             </Map>
         </section>)
+    }
+
+    _editableFG = null
+
+    _onFeatureGroupReady = (reactFGref) => {
+
+        if (reactFGref) {
+
+            // store the ref for future access to content
+    
+            let leafletFG = reactFGref.leafletElement;
+            this._editableFG = reactFGref;
+        }
+    }
+
+    _onChange = () => {
+
+        // this._editableFG contains the edited geometry, which can be manipulated through the leaflet API
+
+        const { onChange } = this.props;
+
+        if (!this._editableFG || !onChange) {
+            return;
+        }
+
+        const geojsonData = this._editableFG.leafletElement.toGeoJSON();
+        onChange(geojsonData);
     }
 
     setLeafletMapRef = map => (this.leafletMap = map && map.leafletElement);
